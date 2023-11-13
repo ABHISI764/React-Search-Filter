41cbb6a9d24da01e2223c6ed6f78a05a00ae7681
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-14 00:26:21 +0530

    React Multi-Search Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
+import Table from './Table';
 import { Users } from './users';
 import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [query, setQuery] = useState("")
   // console.log(Users.filter(user=>user.first_name.toLowerCase().includes(query)));
+  const search = (data) => {
+    return data.filter((item)=> item.first_name.toLowerCase().includes(query) ||
+    item.last_name.toLowerCase().includes(query) || 
+    item.email.toLowerCase().includes(query));
+
+  }
   return (
-    <div className='app'>
+    <div className="app">
       <input type='text'
         placeholder='Search...'
         className='search'
         onChange={(e) => setQuery(e.target.value)}
       />
+      <Table data={search(Users)}/>
 
-      <ul className='list'>
-        {Users.filter((user)=>user.first_name.toLowerCase().includes(query)).map((user, key) => (
-          <li 
-          key={user.id}
-          className='listItem'>{user.first_name}</li>
-        ))}
-      </ul>
     </div>
 
 
